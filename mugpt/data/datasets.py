f8049b6086727f71319b09b1ee41c22b45583bfa
EinfachSim <simsim012321@gmail.com>
2026-05-01 18:46:19 +0200

    fixed ignore issue

---
+++ mugpt/data/datasets.py
@@ -0,0 +1,16 @@
+from torch.utils.data import Dataset
+import numpy as np
+import torch
+
+class BinDataset(Dataset):
+    def __init__(self, path: str, seq_len: int):
+        self.data = np.fromfile(path, dtype=np.uint16)
+        self.seq_len = seq_len
+
+    def __len__(self):
+        return len(self.data) - self.seq_len
+
+    def __getitem__(self, idx):
+        x = torch.from_numpy(self.data[idx:idx+self.seq_len].astype(np.int64))
+        y = torch.from_numpy(self.data[idx+1:idx+self.seq_len+1].astype(np.int64))
+        return x, y
